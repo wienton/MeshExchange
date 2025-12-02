c26678769933d7ca9bcb133d2828b1f2a189f1e6
JustCcode <k13775638@gmail.com>
2025-12-02 17:51:05 +0300

    push project for github repository MeshExchange

--- build/src/main.c
+++ build/src/main.c
@@ -1,6 +1,24 @@
-#include <stdio.h>
-#include <stdlib.h>
+// main.c
+#include "../include/wien.h"
 
 int main(void) {
-    
+    const char* BUILD_FILE = "../../build.wien";
+
+    if (parse_build_file(BUILD_FILE) != 0) {
+        return 1;
+    }
+
+    extern char default_targets[512];
+    if (default_targets[0] != '\0') {
+        char* targets = strdup(default_targets);
+        char* token = strtok(targets, " \t");
+        while (token) {
+            handle_call(token);
+            token = strtok(NULL, " \t");
+        }
+        free(targets);
+    }
+
+    printf("Done.\n");
+    return 0;
 }
